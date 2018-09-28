b7755b0503e5a93eec512f53882afe5eff571b07
Marina Bo <7394959@mail.ru>
2018-09-28 23:48:42 +0300

    added methods and state

--- filter/filter.js
+++ filter/filter.js
@@ -1,14 +1,35 @@
 var Filter = React.createClass({
     
       displayName: 'Filter',
+
+      getInitialState: function() {
+        return { 
+          isFilterActive: false
+        };
+      },
+
+      initialFilter: function(ev) {
+        ev.target.checked 
+          ? this.setState( {isFilterActive:true} )
+          : console.log('off')
+      },
+
+      changeFilter: function(ev) {
+        if (this.state.isFilterActive) {
+          console.log(ev.target.value);
+        }
+      },
     
       render: function() {
-        var wordsBlock=this.props.dataArr.map( option =>
-          React.DOM.div({key: Math.random()} ,
-            React.DOM.span(null, option),
-          )
+        var wordsBlock=this.props.dataArr.map( word =>
+          React.DOM.p({key: Math.random()}, word)
         );
-        return React.DOM.div(null, wordsBlock );
+
+        return React.DOM.div(null,
+          React.DOM.input({type:'checkbox',name:'switchFilter',onChange:this.initialFilter}),
+          React.DOM.input({type:'text',name:'filterInput',onChange:this.changeFilter}),
+          React.DOM.div({className:"wordsWrap"}, wordsBlock),
+        );
         
       },
     
