ccdf1b29c336be73c2be511be70de876997f983c
Marina Bo <7394959@mail.ru>
2018-10-02 00:50:04 +0300

    fixed handlerList

--- filter/filter.js
+++ filter/filter.js
@@ -1,51 +1,51 @@
 var Filter = React.createClass({
-    
-      displayName: 'Filter',
+  
+    displayName: 'Filter',
 
-      propTypes: {
-        dataArr:React.PropTypes.arrayOf(
-          React.PropTypes.string
-        )
-      },
+    propTypes: {
+      dataArr:React.PropTypes.arrayOf(
+        React.PropTypes.string
+      )
+    },
 
-      getInitialState: function() {
-        return { 
-          isSortActive: false,
-          renderedWords: this.props.dataArr,
-          filterStr: ''
-        };
-      },
+    getInitialState: function() {
+      return { 
+        isSortActive: false,
+        renderedWords: this.props.dataArr,
+        filterStr: ''
+      };
+    },
 
-      sortInit: function() {
-        this.setState({ isSortActive: !this.state.isSortActive }, this.handleList );
-      },
+    sortInit: function() {
+      this.setState({ isSortActive: !this.state.isSortActive }, this.handleList );
+    },
 
-      filterInit: function(ev) {
-        this.setState({ filterStr: ev.target.value}, this.handleList );
-      },
+    filterInit: function(ev) {
+      this.setState({ filterStr: ev.target.value}, this.handleList );
+    },
 
-      handleList: function() {
-        var wordsList = this.props.dataArr.slice();
-        if (this.state.isSortActive) {
-          wordsList = wordsList.sort();
-        }
-        if (this.state.filterStr) {
-          wordsList = wordsList.filter(word => word.indexOf(this.state.filterStr)!=-1);
-        }
-        this.setState({renderedWords:wordsList});
-      },
-    
-      render: function() {
-        var wordsBlock=this.state.renderedWords.map( word =>
-          React.DOM.p({key: Math.random()}, word)
-        );
+    handleList: function() {
+      var wordsList = this.props.dataArr.slice();
+      if (this.state.isSortActive) {
+        wordsList = wordsList.sort();
+      }
+      if (this.state.filterStr) {
+        wordsList = wordsList.filter(word => word.indexOf(this.state.filterStr)!=-1);
+      }
+      this.setState({renderedWords:wordsList});
+    },
+  
+    render: function() {
+      var wordsBlock=this.state.renderedWords.map( word =>
+        React.DOM.p({key: Math.random()}, word)
+      );
 
-        return React.DOM.div(null,
-          React.DOM.input({type:'checkbox',defaultChecked:false,name:'switchFilter',onChange:this.sortInit}),
-          React.DOM.input({type:'text',name:'filterInput',onChange:this.filterInit}),
-          React.DOM.div({className:"wordsWrap"}, wordsBlock),
-        );
-        
-      },
-    
-    });
+      return React.DOM.div(null,
+        React.DOM.input({type:'checkbox',defaultChecked:false,name:'switchFilter',onChange:this.sortInit}),
+        React.DOM.input({type:'text',name:'filterInput',onChange:this.filterInit}),
+        React.DOM.div({className:"wordsWrap"}, wordsBlock),
+      );
+      
+    },
+  
+  });
