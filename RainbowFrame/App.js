59affa5b9d21e22f4606057119a84d6d576272bd
Marina Bo <7394959@mail.ru>
2018-10-10 02:15:25 +0300

    created RainbowFrame project

---
+++ RainbowFrame/App.js
@@ -0,0 +1,20 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './App.css';
+import RainbowFrame from './components/RainbowFrame';
+
+let colors = ['blue', 'orange', 'green', 'yellow'];
+
+ReactDOM.render(
+  <div className="Wrapper">
+    <RainbowFrame colorsArr={colors}>
+        <h1>RainbowFrame</h1>
+    </RainbowFrame>
+  </div>
+  
+  , document.getElementById('container') 
+);
+
