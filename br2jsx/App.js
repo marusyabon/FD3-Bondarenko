f2893eaa9fb28e2772422ff3445f29a6cf9a7c80
Marina Bo <7394959@mail.ru>
2018-10-10 00:37:22 +0300

    created br2jsx

---
+++ br2jsx/App.js
@@ -0,0 +1,16 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './App.css';
+import Splitter from './components/Splitter';
+
+
+let str = "Lorem ipsum dolor sit amet, <br>consectetur adipiscing elit, <br />sed do eiusmod tempor incididunt ut labore <br />et dolore magna aliqua. Ut enim ad <br/>minim veniam, quis nostrud exercitation ullamco laboris";
+
+ReactDOM.render(
+  <Splitter text={str} />
+  , document.getElementById('root') 
+);
+
---
+++ br2jsx/components/Splitter.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Splitter extends React.Component {
+
+  static propTypes = {
+    text: PropTypes.string
+  }
+
+  render() {
+  
+  let strArr = this.props.text.split(/<br\s?\/?>/);
+  let joinStr = strArr.map(v =>
+    <p>{v}</p>
+    )
+        
+    return (
+      <div>
+        {joinStr}
+      </div>
+    )
+  }
+}
+
+export default Splitter;
