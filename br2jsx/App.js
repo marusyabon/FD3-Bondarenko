d3f07db0e6ae063439b568a73ddff2b6556f4c31
Marina Bo <7394959@mail.ru>
2018-10-10 00:40:36 +0300

    created component Splitter

--- br2jsx/App.js
+++ br2jsx/App.js
@@ -3,7 +3,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './App.css';
 import Splitter from './components/Splitter';
 
 
--- br2jsx/components/Splitter.js
+++ br2jsx/components/Splitter.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './Splitter.css';
+
 class Splitter extends React.Component {
 
   static propTypes = {
