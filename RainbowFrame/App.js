337710c2dc0be7a46722980f9fdecf5dfb93db52
Marina Bo <7394959@mail.ru>
2018-10-22 01:23:51 +0300

    rewrite with recursion

--- RainbowFrame/App.js
+++ RainbowFrame/App.js
@@ -2,16 +2,43 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
 import './App.css';
-import RainbowFrame from './components/RainbowFrame';
+
+class RainbowFrame extends React.Component {
+
+  static propTypes = {
+    colorsArr: PropTypes.arrayOf(
+        PropTypes.string.isRequired
+    )};
+  
+  render() {
+
+    let colors = this.props.colorsArr,
+        innerContent = this.props.children;
+        console.log(colors.length)
+
+    return (
+      (colors.length > 0) ? (
+        <div className="RainbowFrame" style={{borderColor: colors[0]}}>
+            <RainbowFrame colorsArr={colors.slice(1)}>
+              {innerContent}
+            </RainbowFrame>
+        </div>
+        )
+      : innerContent
+
+    )
+  }
+}
 
 let colors = ['blue', 'orange', 'green', 'yellow'];
 
 ReactDOM.render(
   <div className="Wrapper">
     <RainbowFrame colorsArr={colors}>
-        <h1>RainbowFrame</h1>
+      <p>RainbowFrame</p>
     </RainbowFrame>
   </div>
   
